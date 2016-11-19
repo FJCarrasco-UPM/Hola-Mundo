5937e8aadeafaafcbb05668c85f1c0eff6cd306d
Fran <franciscojose.carrasco.martinez@alumnos.upm.es>
2016-11-19 19:12:45 +0100

    Se añade el proyecto Hola Mundo

---
+++ Trabajo 1 Ingenieria del Software/src/Trabajo 1 Ingeniera del Software.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Trabajo.cpp
+// Author      : Francisco José Carrasco
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello it´s me - XXXX!!" << endl;
+}
